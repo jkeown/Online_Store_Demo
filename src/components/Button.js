b3c8950ba320bf0b085a8c7b5ccdb839e8859327
jkeown <jd.keown@yahoo.com>
2020-10-01 22:19:38 -0700

    edit button component data item url

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ function Button(props){
         <button className="snipcart-add-item bg-yellow-300 border-2 border-blue-800 font-bold text-blue-800 px-3 py-1"
         data-item-id={props.product.product.node.contentfulid}
         data-item-price={props.product.product.node.price}
-        data-item-url="/paintings/starry-night"
+        data-item-url="https://onlinestoredemo.netlify.app/"
         data-item-name={props.product.product.node.name}>
         Add to Cart
         </button>
