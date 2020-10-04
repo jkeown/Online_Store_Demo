d9e2dbcc4486d1ba9b41aa5763a703fa9cc03623
jkeown <jd.keown@yahoo.com>
2020-10-03 20:16:39 -0700

    debugging netlify

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ function Button(props){
         <button className="snipcart-add-item bg-yellow-300 border-2 border-blue-800 font-bold text-blue-800 px-3 py-1"
         data-item-id={props.product.product.node.contentfulid}
         data-item-price={props.product.product.node.price}
-        data-item-url="/"
+        data-item-url="https://onlinestoredemo.netlify.app/{props.product.product.node.name}"
         data-item-name={props.product.product.node.name}>
         Add to Cart
         </button>
