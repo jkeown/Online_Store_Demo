428e8300d81b5d35fb6abdc26d7a0e1c0bd5605d
jkeown <jd.keown@yahoo.com>
2020-10-06 22:41:06 -0700

    cart-items-count padding

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,10 @@ import React from "react"
 
 function Nav() {
   return (
-    <ul className="flex justify-between">
+    <ul className="flex">
       {/* <li>Store</li>
       <li>Contact</li> */}
-      <span class="snipcart-items-count bg-yellow-500 px-2 mr-1 rounded-full"></span>
+      <li class="snipcart-items-count bg-yellow-500 px-2 mr-1 rounded-full">0</li>
       <li class="snipcart-checkout cursor-pointer">Cart</li>
     </ul>
   )
