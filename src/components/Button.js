337d7f33235dcf67ebd1d6abff276064eef3bf56
jkeown <jd.keown@yahoo.com>
2020-10-19 20:32:01 -0700

    data-item-url for snipcart

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Button({product}){
-    return(
+function Button({ product }) {
+    return (
         <button className="snipcart-add-item bg-yellow-300 border-2 border-blue-800 font-bold text-blue-800 px-3 py-1 hover:bg-blue-800 hover:text-yellow-300 hover:border-yellow-300"
-        data-item-id={product.node.contentfulid}
-        data-item-price={product.node.price}
-        data-item-url="https://onlinestoredemo.netlify.app/"
-        data-item-name={product.node.name}>
-        Add to Cart
+            data-item-id={product.node.contentfulid}
+            data-item-price={product.node.price}
+            data-item-url="/"
+            data-item-name={product.node.name}>
+            Add to Cart
         </button>
     )
 }
